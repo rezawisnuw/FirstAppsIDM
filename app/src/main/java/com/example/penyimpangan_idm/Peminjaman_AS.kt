f0e06da3bc4599a2ca2c7b50ff8c209e72309f44
rezawisnuw <rezawisnuwardhana358@gmail.com>
2020-07-24 16:58:18 +0700

    mengubah format alert peminjaman by reza

--- app/src/main/java/com/example/penyimpangan_idm/Peminjaman_AS.kt
+++ app/src/main/java/com/example/penyimpangan_idm/Peminjaman_AS.kt
@@ -238,7 +238,7 @@ class Peminjaman_AS : AppCompatActivity() {
 
                                 val dialog2 = AlertDialog.Builder(this@Peminjaman_AS)
                                 dialog2.setTitle("Submit Gagal!")
-                                dialog2.setMessage("Sudah ada data peminjaman di toko $getInpTokoTujuanbyAS pada tanggal $getInpTglMulaiPinjambyAS hingga tanggal $getInpTglSelesaiPinjambyAS)
+                                dialog2.setMessage("Sudah ada data peminjaman di toko $getInpTokoTujuanbyAS pada tanggal $getInpTglMulaiPinjambyAS hingga tanggal $getInpTglSelesaiPinjambyAS")
                                 dialog2.setNegativeButton("Kembali", DialogInterface.OnClickListener { dialog, which ->
                                     Toast.makeText(
                                         this@Peminjaman_AS,
